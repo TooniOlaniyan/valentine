e8382e8b9f4472c2c3f28619ba21a3a817e72cbe
TooniOlaniyan <asalamlatif@gmail.com>
2025-02-14 10:44:10 +0100

    chnages: new url

--- app/create-link/page.tsx
+++ app/create-link/page.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
 const CreateLinkPage = () => {
-  const valentineLink = `http://localhost:3001?name=${localStorage.getItem(
-    "name"
-  )}`;
+  const [valentineLink, setValentineLink] = useState("");
+
+  useEffect(() => {
+    const name = localStorage.getItem("name") || "unknown";
+    setValentineLink(
+      `https://be0my-valentine.vercel.app/?name=${encodeURIComponent(name)}`
+    );
+  }, []); // Runs only once after mount
 
   const copyLink = () => {
+    if (!valentineLink) return;
+
     navigator.clipboard
       .writeText(valentineLink)
       .then(() => {
@@ -21,14 +27,15 @@ const CreateLinkPage = () => {
   return (
     <div className="flex flex-col items-start justify-center h-screen">
       <h1 className="text-5xl font-bold text-[#F72C5B] mb-4 leading-tight">
-        Send this link to them, <br /> I guarantee they will say YES!! <br /> and
-        let the love begin!
+        Send this link to them, <br /> I guarantee they will say YES!! <br />{" "}
+        and let the love begin!
       </h1>
       <div className="flex flex-col items-start">
-        <p>{valentineLink}</p>
+        <p>{valentineLink || "Loading..."}</p>
         <button
           className="mt-4 w-[20rem] bg-[#3B1C32] hover:bg-[#3b1c32]/50 text-white font-bold py-3 px-4 rounded-2xl"
           onClick={copyLink}
+          disabled={!valentineLink}
         >
           Copy
         </button>
